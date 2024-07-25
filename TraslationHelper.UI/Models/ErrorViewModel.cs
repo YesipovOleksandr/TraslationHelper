93dd1c75622eefd79a24f2be8f16ff190715bfc9
Oleksandr Yesipov <oleksandr.yesipov@avanquest.com>
2024-07-25 20:26:50 +0300

    Created project

---
+++ TraslationHelper.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TraslationHelper.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
